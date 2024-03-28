dc8fb04ff3623467ff322439c62fa64998bd4fec
jrabasedas <70587737+jrabasedas@users.noreply.github.com>
2024-03-28 19:52:25 -0300

    Update searchAgents.py

--- 2. Trabajo Práctico de Búsqueda/search/searchAgents.py
+++ 2. Trabajo Práctico de Búsqueda/search/searchAgents.py
@@ -277,16 +277,25 @@ class CornersProblem(search.SearchProblem):
         self._expanded = 0 # Number of search nodes expanded
 
         "*** YOUR CODE HERE ***"
+        # jmr
+        self.cornersFun = {(1,1):0, (1,top):1, (right, 1):2, (right, top):3}
+        self._visited, self._visitedlist, self._expanded = {}, [], 0
 
     def getStartState(self):
         "Returns the start state (in your state space, not the full Pacman state space)"
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        #util.raiseNotDefined()
+        # jmr
+        return self.startingPosition[0], self.startingPosition[1], (False, False, False, False)
 
     def isGoalState(self, state):
         "Returns whether this search state is a goal state of the problem"
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        #util.raiseNotDefined()
+        # jmr
+        x,y, corners = state
+        return corners == (True, True, True, True)
+        
 
     def getSuccessors(self, state):
         """
@@ -310,6 +319,19 @@ class CornersProblem(search.SearchProblem):
             #   hitsWall = self.walls[nextx][nexty]
 
             "*** YOUR CODE HERE ***"
+            x,y, corners  = state
+            dx, dy = Actions.directionToVector(action)
+            nextx, nexty = int(x + dx), int(y + dy)
+            if not self.walls[nextx][nexty]:
+                if (nextx, nexty) in self.corners:
+                    lcorners = list(corners)
+                    lcorners[self.cornersFun[(nextx, nexty)]] = True
+                    nextCorners = tuple(lcorners)
+                else:
+                    nextCorners = corners
+                nextState = (nextx, nexty, nextCorners)
+                cost = 1
+                successors.append((nextState, action, cost))
 
         self._expanded += 1
         return successors
